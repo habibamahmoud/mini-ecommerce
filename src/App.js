0782e51b431e2cf047ee0c877fe56e0b1de4184e
yomnayasser <yomnayasser31@gmail.com>
2021-09-09 22:04:56 +0200

    info page update

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
    <>
   <NavBar />
-  {/* <Carousel /> */}
+  <Carousel />
    <ProductInfo/>
    </>
   );
--- src/components/ImageComponent.js
+++ src/components/ImageComponent.js
@@ -2,23 +2,36 @@ import "./ImageComponent.css";
 import chairImage from "../images/chair1.png";
 const ImageComponent = () => {
   return (
-      
     <div style={{ backgroundColor: "#B1D5D9" }}>
+      <nav>
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item">
+            Home
+          </li>
+          <li class="breadcrumb-item">
+           Collection 
+          </li>
+          <li class="breadcrumb-item active">Leifrane</li>
+        </ol>
+      </nav>
       <img className="ImageStyle" src={chairImage} />
-        <div class="row">
-          <div class="col-md-6">
-            <div className="leftSide">
-              <p>Dining Chair</p>
-              <h1>Leifrane</h1>
-              <p>$180</p>
-            </div>
+      <div class="d-flex justify-content-around align-items-center">
+        <div>
+          <div class="p-5">
+            <p>Dining Chair</p>
+            <h1>Leifrane</h1>
+            <p>$180</p>
           </div>
         </div>
 
-        <div class="col-md-6">
-        <button  className="btn btn-light"  className="rounded-pill"> <p className="button-txt">Buy</p></button>
+        <div class="p-5">
+          <button className="btn btn-light" className="rounded-pill">
+            {" "}
+            <p className="button-txt">Buy</p>
+          </button>
         </div>
       </div>
+    </div>
   );
 };
 export default ImageComponent;
